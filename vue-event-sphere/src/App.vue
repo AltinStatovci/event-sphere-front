d1dddddb339be74484354c32a8d0b70f74361fd3
Blendi <blendibs2000@gmail.com>
2024-05-27 10:18:20 +0200

    implemented footer

--- vue-event-sphere/src/App.vue
+++ vue-event-sphere/src/App.vue
@@ -1,14 +1,33 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-<nav>
-  <Header/>
-</nav>
+  <div id="app" class="d-flex flex-column min-vh-100">
+    <nav>
+      <Header/>
+    </nav>
 
-  <RouterView />
+    <main class="flex-fill">
+      <RouterView />
+    </main>
+
+    <footer>
+      <Footer/>
+    </footer>
+  </div>
 </template>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+main {
+  flex: 1;
+}
+</style>
 
---
+++ vue-event-sphere/src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <MDBFooter :text="['center', 'lg-start']">
+      <!-- Grid container -->
+      <MDBContainer class="p-4">
+        <!--Grid row-->
+        <MDBRow>
+          <!--Grid column-->
+          <MDBCol md="12" class="mb-4 mb-md-0">
+            <h5 class="text-uppercase">Event Sphere</h5>
+  
+            <p>
+                Welcome to EventSphere, your ultimate destination for unforgettable experiences!
+                 Whether you're a music enthusiast eager to catch your favorite artist live,
+                  a sports fan seeking thrilling game-day action, or an adventure lover looking for exciting outdoor activities,
+                   we have you covered. Our easy-to-use platform allows you to browse and purchase tickets for concerts, sports events,
+                    and a variety of outdoor adventures. Discover upcoming events, secure your seats, and get ready to create lasting memories with EventSphere.
+                     Start your journey today and never miss out on the excitement!
+            </p>
+          </MDBCol>
+        </MDBRow>
+        <!--Grid row-->
+      </MDBContainer>
+      <!-- Grid container -->
+  
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2024 Copyright:
+      </div>
+      <!-- Copyright -->
+    </MDBFooter>
+  </template>
+
+<script setup lang="ts">
+    import {
+    MDBFooter,
+    MDBContainer,
+    MDBRow,
+    MDBCol
+    } from 'mdb-vue-ui-kit';
+</script>
