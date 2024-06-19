810d6e27db408761c83c4fd25e9eeb5f9cb41e2b
Altin Statovci <altinstatovci56@gmail.com>
2024-06-19 11:05:54 +0200

    manage accounts

---
+++ vue-event-sphere/src/store/userStore.js
@@ -0,0 +1,79 @@
+import { defineStore } from "pinia";
+import { ref, toRaw } from "vue";
+import client from "@/helpers/client.js";
+import { useAuthStore } from "@/store/authStore.js";
+
+export const useUserStore = defineStore('user', () => {
+    const authStore = useAuthStore();
+    const url = 'http://localhost:5220/api/';
+
+    const user = ref(null);
+    const users = ref([]);
+    const userCount = ref(0);
+    const roles = ref([]);
+
+
+    const getRoles = async () =>{
+        try {
+            const response = await client.get(`${url}roles`)
+            roles.value = response.data;
+            return roles.value
+        }catch (error){
+            console.error('Error getting roles:', error);
+            throw error
+        }
+    }
+
+    const getUsers = async () => {
+        try {
+            const response = await client.get(`${url}User/getUsers`);
+            users.value = response.data; // Ensure plain object
+            return users.value;
+        } catch (error) {
+            console.error('Error getting users:', error);
+            throw error;
+        }
+    };
+
+    const getUserCount = async () => {
+        try {
+            const response = await client.get(`${url}User/count`);
+            userCount.value = response.data; // Ensure plain object
+            return userCount.value;
+        } catch (error) {
+            console.error('Error getting user count:', error);
+            throw error;
+        }
+    };
+
+    const getUser = async (id) => {
+        try {
+            const response = await client.get(`${url}User/getUser/${id}`);
+            user.value = response.data; // Ensure plain object
+            return user.value;
+        } catch (error) {
+            console.error(`Error getting user with id ${id}:`, error);
+            throw error;
+        }
+    };
+
+    const updateUser = async (userUpdate) => {
+        try {
+            await client.put(`${url}User/updateUser`, userUpdate);
+        } catch (error) {
+            console.error('Error updating user:', error);
+            throw error;
+        }
+    };
+
+    const deleteUser = async (id) => {
+        try {
+            await client.delete(`${url}User/deleteUser/${id}`);
+        } catch (error) {
+            console.error(`Error deleting user with id ${id}:`, error);
+            throw error;
+        }
+    };
+
+    return { getUsers, getUserCount, getUser, updateUser, deleteUser, getRoles, user, users, userCount, roles };
+});
--- vue-event-sphere/src/views/Profile.vue
+++ vue-event-sphere/src/views/Profile.vue
@@ -1,239 +1,206 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import { useStore } from 'vuex';
+import { useUserStore } from "@/store/userStore.js";
+import { useAuthStore } from "@/store/authStore.js";
 import SideBar from "@/components/SideBar.vue";
-
-const user = ref({
-    firstName: 'Valerie',
-    lastName: 'Luna',
-    email: 'name@example.com',
-    phone: '555-123-4567',
-    birthday: '06/10/1988'
-});
+import UpdateRoleModalComponent from "@/components/modal/UpdateRoleModalComponent.vue";
+import Swal from "sweetalert2"; // Import the modal component
 
 const activeTab = ref('profile');
-const showPasswordChangeForm = ref(false);
-const oldPassword = ref('');
-const newPassword = ref('');
-const confirmNewPassword = ref('');
+const showModal = ref(false); // State to manage modal visibility
+const selectedUser = ref(null); // State to manage selected user
 
-const store = useStore();
+const authStore = useAuthStore();
+const id = authStore.id; // Get the authenticated user's ID
+const userStore = useUserStore();
+const users = ref([]);
 
-onMounted(() => {
-    fetchUserDetails();
+const user = ref({
+  id: id,
+  name: '',
+  lastName: '',
+  email: '',
+  password: '',
+  roleID: '',
+  roleName: '',
 });
 
 const fetchUserDetails = async () => {
-    try {
-        // Fetch user details from the server
-        // Assign fetched user data to the user ref
-    } catch (error) {
-        console.error('Error fetching user details:', error.message);
+  try {
+    const response = await userStore.getUser(id);
+    user.value = response;
+  } catch (error) {
+    console.error('Error fetching user details:', error.message);
+  }
+};
+
+const fetchAllUsers = async () => {
+  try {
+    const response = await userStore.getUsers();
+    users.value = response;
+  } catch (error) {
+    console.error('Error fetching all users:', error.message);
+  }
+};
+
+const editProfile = async () => {
+  try {
+    await userStore.updateUser(user.value);
+    await Swal.fire({
+      title: "User updated successfully!",
+      text: "Please login!",
+      icon: "success"
+    }).then(() => {
+      location.reload();
+    });
+  } catch (error) {
+    console.error('Error updating profile:', error.message);
+  }
+};
+
+const deleteUser = async (userId) => {
+  try {
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    });
+
+    if (result.isConfirmed) {
+      await userStore.deleteUser(userId);
+      await Swal.fire({
+        title: "User Deleted successfully!",
+        icon: "success"
+      }).then(() => {
+        window.location.reload();
+      });
     }
+  } catch (error) {
+    console.error('Failed to delete user:', error);
+  }
 };
 
-const editProfile = () => {
-    // Redirect or show edit profile form
-};
-
-const deleteProfile = () => {
-    // Show confirmation modal for profile deletion
+// Open modal and set selected user
+const openModal = (user) => {
+  selectedUser.value = { ...user }; // Ensure deep copy to avoid mutations
+  showModal.value = true;
 };
 
 const changeTab = (tab) => {
-    activeTab.value = tab;
-    if (tab === 'security') {
-        showPasswordChangeForm.value = true;
-    } else {
-        showPasswordChangeForm.value = false;
-    }
+  activeTab.value = tab;
 };
 
-const changePassword = () => {
-    if (newPassword.value !== confirmNewPassword.value) {
-        alert('New passwords do not match!');
-        return;
-    }
-    // Proceed with password change logic
-    console.log('Password changed successfully');
-};
+onMounted(async () => {
+  await fetchUserDetails();
+  await fetchAllUsers();
+});
 </script>
 
 <template>
-    <div class="d-flex">
-        <side-bar />
-        <div class="container-xl px-4 mt-4">
-            <nav class="nav nav-borders">
-                <a class="nav-link" :class="{ active: activeTab === 'profile' }"
-                    @click.prevent="changeTab('profile')">Profile</a>
-                <a class="nav-link" :class="{ active: activeTab === 'security' }"
-                    @click.prevent="changeTab('security')">Security</a>
-            </nav>
-            <hr class="mt-0 mb-4">
+  <div class="d-flex">
+    <side-bar />
+    <div class="container-xl px-4 mt-4">
+      <nav class="nav nav-borders">
+        <a class="nav-link" :class="{ active: activeTab === 'profile' }" @click.prevent="changeTab('profile')">Profile</a>
+        <a class="nav-link" :class="{ active: activeTab === 'Admin' }" @click.prevent="changeTab('Admin')">Admin</a>
+      </nav>
+      <hr class="mt-0 mb-4">
 
-            <div class="row" v-if="activeTab === 'profile'">
-                <div class="col-xl-4">
-                    <div class="card mb-4 mb-xl-0">
-                        <div class="card-header">Profile Picture</div>
-                        <div class="card-body text-center">
-                            <img class="img-account-profile rounded-circle mb-2"
-                                src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
-                            <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
-                            <button class="btn btn-primary" type="button">Upload new image</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-8">
-                    <div class="card mb-4">
-                        <div class="card-header">Account Details</div>
-                        <div class="card-body">
-                            <form>
-                                <div class="row gx-3 mb-3">
-                                    <div class="col-md-6">
-                                        <label class="small mb-1" for="inputFirstName">First name</label>
-                                        <input class="form-control" id="inputFirstName" type="text"
-                                            placeholder="Enter your first name" v-model="user.firstName">
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label class="small mb-1" for="inputLastName">Last name</label>
-                                        <input class="form-control" id="inputLastName" type="text"
-                                            placeholder="Enter your last name" v-model="user.lastName">
-                                    </div>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                                    <input class="form-control" id="inputEmailAddress" type="email"
-                                        placeholder="Enter your email address" v-model="user.email">
-                                </div>
-                                <div class="row gx-3 mb-3">
-                                    <div class="col-md-6">
-                                        <label class="small mb-1" for="inputPhone">Phone number</label>
-                                        <input class="form-control" id="inputPhone" type="tel"
-                                            placeholder="Enter your phone number" v-model="user.phone">
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label class="small mb-1" for="inputBirthday">Birthday</label>
-                                        <input class="form-control" id="inputBirthday" type="text" name="birthday"
-                                            placeholder="Enter your birthday" v-model="user.birthday">
-                                    </div>
-                                </div>
-                                <button class="btn btn-primary" type="button" @click="editProfile">Save changes</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+      <!-- Profile Tab -->
+      <div class="row" v-if="activeTab === 'profile'">
+        <div class="col-xl-4">
+          <div class="card mb-4 mb-xl-0">
+            <div class="card-header">Profile Picture</div>
+            <div class="card-body text-center">
+              <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+              <div class="small font-italic text-muted mb-4">{{user.name}} {{user.lastName}}</div>
+              <button class="btn btn-primary" type="button">Upload new image</button>
             </div>
-
-            <transition name="slide-fade">
-                <div v-if="showPasswordChangeForm" class="card mb-4">
-                    <div class="card-header">Change Password</div>
-                    <div class="card-body">
-                        <form @submit.prevent="changePassword">
-                            <div class="mb-3">
-                                <label class="small mb-1" for="oldPassword">Old Password</label>
-                                <input class="form-control" id="oldPassword" type="password" v-model="oldPassword"
-                                    required>
-                            </div>
-                            <div class="mb-3">
-                                <label class="small mb-1" for="newPassword">New Password</label>
-                                <input class="form-control" id="newPassword" type="password" v-model="newPassword"
-                                    required>
-                            </div>
-                            <div class="mb-3">
-                                <label class="small mb-1" for="confirmNewPassword">Confirm New Password</label>
-                                <input class="form-control" id="confirmNewPassword" type="password"
-                                    v-model="confirmNewPassword" required>
-                            </div>
-                            <button class="btn btn-primary" type="submit">Change Password</button>
-                        </form>
-                    </div>
-                </div>
-            </transition>
+          </div>
         </div>
+        <div class="col-xl-8">
+          <div class="card mb-4">
+            <div class="card-header">Account Details</div>
+            <div class="card-body">
+              <form>
+                <div class="row gx-3 mb-3">
+                  <div class="col-md-6">
+                    <label class="small mb-1" for="inputFirstName">First name</label>
+                    <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" v-model="user.name">
+                  </div>
+                  <div class="col-md-6">
+                    <label class="small mb-1" for="inputLastName">Last name</label>
+                    <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" v-model="user.lastName">
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <label class="small mb-1" for="inputEmailAddress">Email address</label>
+                  <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" v-model="user.email">
+                </div>
+
+                <div class="mb-3">
+                  <label class="small mb-1" for="inputPassword">Password</label>
+                  <input class="form-control" id="inputPassword" type="password" placeholder="Enter your password" v-model="user.password">
+                </div>
+
+                <div class="mb-3">
+                  <label class="small mb-1" for="inputRole">Role</label>
+                  <input class="form-control" id="inputRole" type="text" :disabled="true" v-model="user.roleName">
+                </div>
+
+                <button class="btn btn-primary" type="button" @click="editProfile">Save changes</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Admin Tab -->
+      <div v-if="activeTab === 'Admin'">
+        <div class="card mb-4">
+          <div class="card-header">All Users</div>
+          <div class="card-body">
+            <table class="table">
+              <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th></th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="user in users" :key="user.id">
+                <td>{{ user.name }}</td>
+                <td>{{ user.lastName }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.roleName }}</td>
+                <td class="text-end">
+                  <button class="btn btn-outline-danger btn-sm m-2" :disabled="user.id == id" @click="deleteUser(user.id)">Delete</button>
+                  <button class="btn btn-primary btn-sm" :disabled="user.id == id" @click="openModal(user)">
+                    Update User Role
+                  </button>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+
+  <!-- Include the modal component and pass necessary props -->
+  <update-role-modal-component :show-modal="showModal" :user="selectedUser" @close="showModal = false" />
 </template>
 
 <style scoped>
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
 
-body {
-    margin-top: 20px;
-    background-color: #f2f6fc;
-    color: #69707a;
-}
-
-.img-account-profile {
-    height: 10rem;
-}
-
-.rounded-circle {
-    border-radius: 50% !important;
-}
-
-.card {
-    box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
-}
-
-.card .card-header {
-    font-weight: 500;
-}
-
-.card-header:first-child {
-    border-radius: 0.35rem 0.35rem 0 0;
-}
-
-.card-header {
-    padding: 1rem 1.35rem;
-    margin-bottom: 0;
-    background-color: rgba(33, 40, 50, 0.03);
-    border-bottom: 1px solid rgba(33, 40, 50, 0.125);
-}
-
-.form-control,
-.dataTable-input {
-    display: block;
-    width: 100%;
-    padding: 0.875rem 1.125rem;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1;
-    color: #69707a;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid #c5ccd6;
-    appearance: none;
-    border-radius: 0.35rem;
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
-
-.nav-borders .nav-link.active {
-    color: #0061f2;
-    border-bottom-color: #0061f2;
-}
-
-.nav-borders .nav-link {
-    color: #69707a;
-    border-bottom-width: 0.125rem;
-    border-bottom-style: solid;
-    border-bottom-color: transparent;
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-    padding-left: 0;
-    padding-right: 0;
-    margin-left: 1rem;
-    margin-right: 1rem;
-    cursor: pointer;
-}
-
-.slide-fade-enter-active,
-.slide-fade-leave-active {
-    transition: opacity 0.5s, transform 0.5s;
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-    opacity: 0;
-    transform: translateX(10px);
-}
+/* Styles same as provided previously */
 </style>
