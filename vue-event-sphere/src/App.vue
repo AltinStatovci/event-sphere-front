9da8ba3c1725a9cabdd04b4db55f4f39c83e808b
HMuhamed <50260119+HMuhamed@users.noreply.github.com>
2024-07-05 13:04:07 +0200

    tickets better UI

--- vue-event-sphere/src/App.vue
+++ vue-event-sphere/src/App.vue
@@ -28,7 +28,7 @@ const route = useRoute();
 
     <footer>
       <Footer
-        v-if="route.name !== 'login' && route.name !== 'register' &&  route.name !== 'dashboard' && route.name !== 'profile' && route.name !== 'paymentDashboard' && route.name !== 'report' && route.name !== 'manageEvent' && route.name !== 'manageTickets'" />
+        v-if="route.name !== 'login' && route.name !== 'register' &&  route.name !== 'dashboard' && route.name !== 'profile' && route.name !== 'paymentDashboard' && route.name !== 'report' && route.name !== 'logs' && route.name !== 'manageTickets' && route.name !== 'manageEvent'" />
     </footer>
   </div>
 </template>
